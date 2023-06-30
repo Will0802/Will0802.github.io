5920204da6cea87e2a81e8f8551c3a0acf0bb976
Will <t6111221@gmail.com>
2023-06-30 18:17:43 +0800

    簡易1A2B

---
+++ 1A2B/test.js
@@ -0,0 +1,17 @@
+let input = "1234";
+let ans = "1234";
+let a = 0;
+let b = 0;
+let newArr = [];
+let inputArr = input.split("");
+console.log(inputArr);
+inputArr.forEach((n) => {
+  newArr.push(n);
+});
+console.log(newArr);
+
+// for (let i = 0; i < 4; i++) {
+//   if (input[i] == ans[i]) {
+//     a++;
+//   }
+// }
